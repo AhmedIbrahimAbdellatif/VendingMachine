b66a04aa5df42713610fd96ebc519eb75ccdc7a2
AhmedIbrahimAbdellatif <Ahmed.Ahmed99@eng-st.cu.edu.eg>
2021-10-08 14:22:40 +0200

    First Commit

---
+++ src/main/java/com/vendingmachine/kata/VendingMachineKataApplication.java
@@ -0,0 +1,13 @@
+package com.vendingmachine.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendingMachineKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendingMachineKataApplication.class, args);
+	}
+
+}
